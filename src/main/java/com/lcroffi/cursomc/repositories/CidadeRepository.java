02cd3dd3ec72fb68d8c297dc71ebac1e4af345b8
lcroffi <lileekat@hotmail.com>
2018-07-25 10:51:40 -0300

    Estado e cidade

---
+++ src/main/java/com/lcroffi/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.lcroffi.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lcroffi.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
