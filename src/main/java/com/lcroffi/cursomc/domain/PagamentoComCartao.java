c2d54a6d99e026af68c7e226535e6c477087bdd5
lcroffi <lileekat@hotmail.com>
2018-07-31 17:47:07 -0300

    Inserindo Pedido

--- src/main/java/com/lcroffi/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/lcroffi/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.lcroffi.cursomc.domain;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.lcroffi.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
