67aa4152bf04fe1637ee19bec6ae73713afadbc8
lcroffi <lileekat@hotmail.com>
2018-12-20 10:25:29 -0200

    Povoando dinamicamente estado e cidade

--- SpringIonic/src/pages/registrar/registrar.module.ts
+++ SpringIonic/src/pages/registrar/registrar.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { RegistrarPage } from './registrar';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service';
 
 @NgModule({
   declarations: [
@@ -9,5 +11,9 @@ import { RegistrarPage } from './registrar';
   imports: [
     IonicPageModule.forChild(RegistrarPage),
   ],
+  providers: [
+    CidadeService,
+    EstadoService
+  ]
 })
 export class RegistrarPageModule {}
