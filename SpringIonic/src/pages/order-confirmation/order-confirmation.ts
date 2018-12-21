3c973e17f9dd874868080da891cdd9ef42fd79c9
lcroffi <lileekat@hotmail.com>
2018-12-21 10:47:53 -0200

    Informando pedido registrado ao usuario

--- SpringIonic/src/pages/order-confirmation/order-confirmation.ts
+++ SpringIonic/src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -28,6 +29,10 @@ export class OrderConfirmationPage {
     public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
+  }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
   ionViewDidLoad() {
@@ -60,12 +65,17 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
       .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codpedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403) {
           this.navCtrl.setRoot('HomePage');
         }
       });
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
